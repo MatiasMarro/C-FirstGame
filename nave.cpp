3600b5b5859a14116743edc597c06236a755faf1
MatiasMarro <matiasmarro1987@gmail.com>
2021-10-13 11:22:03 -0300

    Create object asteroid

--- nave.cpp
+++ nave.cpp
@@ -2,6 +2,7 @@
 #include <windows.h>
 #include <conio.h>
 #include <wincon.h>
+#include <stdlib.h>
 
 #define ARRIBA 72
 #define IZQUIERDA 75
@@ -50,12 +51,22 @@ void pintarLimites(){
 class NAVE{
     int x, y;
     int corazones;
+    int vidas;
 public:
-    NAVE(int _x, int _y, int _corazones): x(_x), y(_y), corazones(_corazones) {/* //pasando los datos al constructor */}
+    NAVE(int _x, int _y, int _corazones, int _vidas): x(_x), y(_y), corazones(_corazones), vidas (_vidas) {/* //pasando los datos al constructor */}
     void pintar();
     void borrar();
     void mover();
     void pintar_corazones();
+    void morir();
+};
+
+class AST{
+    int x,y;
+public:
+    AST(int _x, int _y): x(_x), y(_y){/* //se pasan los datos al cosntructor */}
+    void pintar();
+    void mover();
 };
 
 void NAVE::pintar(){
@@ -64,10 +75,10 @@ void NAVE::pintar(){
     gotoxy(x,y+2); printf("%c%c%c %c",30,190,190,30);
 }
 
-void NAVE::borrar(){
-    gotoxy(x,y);   printf("     ");
-    gotoxy(x,y+1); printf("     ");
-    gotoxy(x,y+2); printf("     ");
+void NAVE::borrar(){ 
+    gotoxy(x,y);   printf("            ");
+    gotoxy(x,y+1); printf("            ");
+    gotoxy(x,y+2); printf("            ");
 }
 
 void NAVE::mover(){
@@ -88,6 +99,7 @@ void NAVE::mover(){
 }
 
 void NAVE::pintar_corazones(){
+    gotoxy(50,2); printf("Vidas: %d",vidas);
     gotoxy(64,2); printf("Salud:");
     gotoxy(70,2); printf("      ");
     for(int i=0;i<corazones; i++){
@@ -97,24 +109,66 @@ void NAVE::pintar_corazones(){
     }
 }
 
+void NAVE::morir(){
+    if(corazones == 0){
+        borrar();
+        gotoxy(x,y);  printf("   **    ");
+        gotoxy(x,y+1);printf(" ******  ");
+        gotoxy(x,y+2);printf("   **    ");
+        Sleep(200);
+
+        borrar();
+        gotoxy(x,y);  printf("*   **    *");
+        gotoxy(x,y+1);printf("  *******  ");
+        gotoxy(x,y+2);printf("*   **    *");
+        Sleep(200);
+
+
+        vidas --;
+        corazones = 3;
+        pintar_corazones();
+        borrar();
+        pintar();
+        
+        
+    }
+}
+
+void AST::pintar(){
+    gotoxy(x,y);printf("%c",184);
+}
+
+void AST::mover(){
+    gotoxy(x,y); printf(" ");
+    y++;
+    if(y>32){
+        x = rand() % 71 +4;
+        y = 4;
+    }
+    pintar(); 
+}
+
 
 int main(){
     pintarLimites();
     OcultarCursor();
-    NAVE N(7,7,3);
+    NAVE N(7,7,3,3);
+    AST ast(10,4);
+
 
     N.pintar();
     N.pintar_corazones();
     
-    bool gameover = false ;
-    while(!gameover){
 
+    bool gameover = false ;
+
+    while(!gameover){
+        ast.mover();
+        N.morir();
         N.mover();
-             Sleep(10);
+             Sleep(30);
     }
 
 
-    
-    
     return  0;
 }
